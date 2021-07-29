e28146f1dcfafb10606bbdbfc70c35e2d0d112e9
Brittany Richards <brittany.m.richards3@gmail.com>
2021-07-29 15:58:51 -0500

    exercises

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 30;
+        System.out.println(myFavoriteNumber);
+
+
+
+
+
+
+    }
+}
