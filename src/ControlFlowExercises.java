bedbe53eb531f437d38ddb0ef14e52aa74734d84
Brittany Richards <brittany.m.richards3@gmail.com>
2021-08-02 17:00:31 -0500

    2 in progress

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,7 +1,57 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for (int i = 5; i <= 15 ; i++) {
-            System.out.println(i);
+
+//        //1a
+//            int i = 5;
+//            while (i <= 15) {
+//                System.out.print(i++ + " ");
+//            }
+            //1C for loop
+//            for (int i = 5; i <= 15; i++) {
+//                System.out.println(i);
+//            }
+
+            //1b.a
+//            int i = 0;
+//            do {
+//                System.out.println(i);
+//                i += 2;
+//            }
+//            while (i <= 100);
+//
+//            //1b.b
+//            int i = 100;
+//            do {
+//                System.out.println(i);
+//                i -= 5;
+//            }
+//            while (i >= -10);
+//
+//            //1.b.c
+//            long i = 2;
+//            do {
+//                System.out.println(i);
+//                i = i * i;
+//            }
+//            while (i < 100000);
+//
+//            //for Loop
+//            for (long i = 2; i < 1000000; i *= i) {
+//                System.out.println(i);
+//            }
+            //2. Fizzbuzz
+        for (int i = 1; i <= 100 ; i++) {
+            if (i % 3 == 0){
+                System.out.println();
+            }
+        }
         }
     }
-}
+
+  for (int i = 0; i < 100; i++) {
+//            if (i == 23){
+//                System.out.println("Skipping 23.");
+//                continue;
+//            }
+//            System.out.println(i);
+//        }
