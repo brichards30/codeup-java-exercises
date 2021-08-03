a74a1b688d387352bb12ba2893312cc585a2e2b8
Brittany Richards <brittany.m.richards3@gmail.com>
2021-08-03 15:46:36 -0500

    Methods Exercise initial commit

--- src/MethodsLecture.java
+++ src/MethodsLecture.java
@@ -1,13 +1,54 @@
+import java.util.Scanner;
+
 public class MethodsLecture {
 
 //DECLARE METHODS INSIDE OF THE CLASS, OUTSIDE MAIN
 
-    public static
+    static double piValue = 3.14;
 
-    //THIS EXECUTES CODE. METHODS SHOULD NOT BE DECLARED INSIDE MAIN
-    public static void main(String[] args) {
+    public static String sayHello(String userName) {
+        String helloMessage = String.format("Hello, %s!", userName);
+        return helloMessage;
+    }
+
+    public static double getAreaOfCircle(float radius) {
+        return piValue * (radius * radius);
+    }
+
+    public static double getAreaOfCircle(double radius) {
+        return piValue * (radius * radius);
+    }
+
+    public static double getAreaOfCircle(long radius) {
+        return piValue * (radius * radius);
+    }
+
+    public static boolean doesUserWantToContinue() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Would you like to continue?");
+
+        String userResponse = input.nextLine();
+
+        if (userResponse.equals("yes")) {
+            return true;
+        } else if (userResponse.equals("no")) {
+            return false;
+        } else {
+            return doesUserWantToContinue();
+        }
 
     }
 
+    //THIS EXECUTES CODE. METHODS SHOULD NOT BE DECLARED INSIDE MAIN
+    public static void main(String[] args) {
+        if (doesUserWantToContinue()) {
+            System.out.println(sayHello("Jett"));
 
+            int radiusOfCircle = 4;
+
+            System.out.println(getAreaOfCircle(radiusOfCircle));
+
+        }
+    }
 }
