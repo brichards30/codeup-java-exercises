47f441b8e408df3b34c7a14e63cdf1c837fa0a5b
Brittany Richards <brittany.m.richards3@gmail.com>
2021-08-03 11:56:05 -0500

    Bob complete

---
+++ src/Bob.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+
+        //Answers 'Sure' if input ends in ?
+        //Answers 'Whoa, chill out!' if input ends with !
+        //Answers 'Fine. Be that way!' if response if empty
+        //Answers 'Whatever' to anything else
+
+        Scanner bobReactions = new Scanner(System.in);
+
+        boolean answerQuestion = true;
+
+        do {
+
+            System.out.println("What do you want to say to Bob?");
+            String userResponse = bobReactions.nextLine().trim();
+
+            if (userResponse.endsWith("?")) {
+                System.out.println("Sure.");
+            } else if (userResponse.endsWith("!")) {
+                System.out.println("Whoa, chill out!");
+            } else if (userResponse.isEmpty()) {
+                System.out.println("Fine. Be that way!");
+            } else {
+                System.out.println("Whatever.");
+            }
+            System.out.println("Do you want to say anything else? (y/n)");
+
+            String userNewResponse = bobReactions.next();
+
+            if (!userNewResponse.equalsIgnoreCase("y")){
+                answerQuestion = false;
+                System.out.println("See you later then.");
+            }
+
+        } while (answerQuestion);
+    }
+}
