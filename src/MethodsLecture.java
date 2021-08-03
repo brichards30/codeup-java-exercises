1060de1e753bedd8d362ac60309726ed496e05d9
Brittany Richards <brittany.m.richards3@gmail.com>
2021-08-03 14:21:11 -0500

    initial commit

---
+++ src/MethodsLecture.java
@@ -0,0 +1,13 @@
+public class MethodsLecture {
+
+//DECLARE METHODS INSIDE OF THE CLASS, OUTSIDE MAIN
+
+    public static
+
+    //THIS EXECUTES CODE. METHODS SHOULD NOT BE DECLARED INSIDE MAIN
+    public static void main(String[] args) {
+
+    }
+
+
+}
