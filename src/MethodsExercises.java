e8f453c6308980afe4e922c0bd020e20e1c83072
Brittany Richards <brittany.m.richards3@gmail.com>
2021-08-04 08:44:21 -0500

    created methods? Need assistance

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,2 +1,26 @@
 public class MethodsExercises {
+
+    int addition(int num1, int num2){
+        return num1 + num2;
+    }
+
+    int subtraction(int num1, int num2){
+        return num2 - num1;
+    }
+
+    int multiply(int num1, int num2){
+        return num1 * num2;
+    }
+
+    int divide(int num1, int num2){
+        return num2 / num1;
+    }
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+
 }
