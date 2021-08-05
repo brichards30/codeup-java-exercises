0af02525b7871e9db96d8aa9a0fc982ad45daffb
Brittany Richards <brittany.m.richards3@gmail.com>
2021-08-05 10:20:36 -0500

    lesson in progress, return after resume workshop

--- src/Person.java
+++ src/Person.java
@@ -36,6 +36,9 @@ public class Person {
 
        Person.abbreviateFirstName("Bob");
 
+       Person tom = new Person();
+        System.out.println(tom.getFullName());
+
 
 
 //        System.out.println(abbreviateFirstName());
