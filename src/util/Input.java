92eb1749223dba78a46a30a6882434e55ddac18c
Brittany Richards <brittany.m.richards3@gmail.com>
2021-08-06 10:41:47 -0500

    updated with Ry's note

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,60 +1,63 @@
 package util;
+
 import java.util.Scanner;
 
 public class Input {
+    //From Ry's walkthru
+//    public class Input{
+//    private Scanner scanner;
+//    public Input(){
+//    this.scanner = new Scanner(System.in);
+//    }
+
     private Scanner scanner = new Scanner(System.in);
 
 
-
-    public String getString(){
+    public String getString() {
         System.out.println("Say something: ");
         String userInput = scanner.nextLine();
         return userInput;
-
-
     }
 
-    public boolean yesNo(){
+    public boolean yesNo() {
         System.out.println("Do you like pizza (y/n)?");
         String userResponse = scanner.nextLine();
-        if (userResponse.equals("yes") || userResponse.equals("y")){
+        if (userResponse.equals("yes") || userResponse.contains("y")) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
-    public int getInt(int min, int max){
-        System.out.println("Enter a number: ");
+    public int getInt(int min, int max) {
+        System.out.println("See if your number is within the range: ");
         int userResponse = scanner.nextInt();
         if (userResponse >= min && userResponse <= max) {
+            System.out.println("Dust your self off and try again!");
+            return getInt(min, max);
+        } else {
             System.out.println("Yay! " + userResponse + " is within the range!");
             return userResponse;
-        }else{
-            return getInt(min, max);
         }
-
     }
-
-    public int getInt(){
+    public int getInt() {
         System.out.println("Enter a whole number: ");
         int usersInteger = scanner.nextInt();
         return usersInteger;
     }
 
-    public double getDouble (double min, double max){
-        System.out.println("Enter a number with a decimal: ");
+    public double getDouble(double min, double max) {
+        System.out.println("See if your number with a decimal is within range: ");
         double userInput = scanner.nextDouble();
         if (userInput >= min && userInput <= max) {
+            System.out.println("Try again.");
+            return getDouble(min, max);
+        } else {
             System.out.println("Yay! " + userInput + " is within the range!");
             return userInput;
-        }else{
-            System.out.printf("Try again.");
-            return getDouble(min, max);
         }
     }
-
-    public double getDouble(){
+    public double getDouble() {
         System.out.println("Enter a number with a decimal: ");
         double userNumber = scanner.nextDouble();
         return userNumber;
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -6,7 +6,7 @@ public class InputTest {
         Input input = new Input();
         System.out.println(input.getString());
         System.out.println(input.yesNo());
-        System.out.println(input.getInt(0, 15));
+        System.out.println(input.getInt(1, 15));
         System.out.println(input.getInt());
         System.out.println(input.getDouble(1.0, 20.0));
         System.out.println(input.getDouble());
