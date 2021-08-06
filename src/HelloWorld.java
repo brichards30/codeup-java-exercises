9345629d3f192b23dff56ab14901a3e416d112b9
Brittany Richards <brittany.m.richards3@gmail.com>
2021-08-06 16:14:04 -0500

    working

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -10,6 +10,41 @@ public class HelloWorld {
         String myString = "Hey, there!";
         System.out.println(myString);
 
+//        myString = 3.14159;
+//        4. System.out.println(myString);//incompatible types
+
+//        long myNumber = 123L;
+        float myNumber = 3.14f;
+//        myNumber = 123;
+        System.out.println(myNumber);
+        //variable not initialized
+        //63.14 lossy conversion from double to long
+        //8. 123 printed
+        //9. 3.14 is a double, or add f
+
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+////        int three = (int) o;//incompatible types
+//        int three = (int) "three";//incompatible types
+
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+
+
+
     }
 }
 
