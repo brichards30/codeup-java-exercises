249aba391b3570662e8fbd6c46c707632ba69066
Brittany Richards <brittany.m.richards3@gmail.com>
2021-08-06 15:40:12 -0500

    ShapesTest complete

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -19,4 +19,19 @@ public class Rectangle {
         return area;
     }
 
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
 }
--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -1,4 +1,18 @@
 package shapes;
 
+
 public class ShapesTest {
+    public static void main(String[] args) {
+        Rectangle box1 = new Rectangle(4,5);
+        System.out.println("The perimeter of the rectangle is: " + box1.getPerimeter());
+        System.out.println();
+        System.out.println("The area of the rectangle is: " + box1.getArea());
+
+        System.out.println("-------------");
+
+        Rectangle box2 = new Square(5);
+
+        System.out.println("The perimeter of the square is: " + box2.getPerimeter());
+        System.out.printf("The area of the square is: " + box2.getArea());
+    }
 }
