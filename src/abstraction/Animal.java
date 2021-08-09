6cf02079bf1ae9dc63f4f9f546373ae3fb8eb52b
Brittany Richards <brittany.m.richards3@gmail.com>
2021-08-09 11:01:28 -0500

    initial commit

---
+++ src/abstraction/Animal.java
@@ -0,0 +1,10 @@
+package abstraction;
+
+public abstract class Animal {
+
+    public void walk() {
+        System.out.println("Walks around a bit");
+    }
+
+    public final void breathe(int numSeconds)
+}
---
+++ src/abstraction/AnimalFarm.java
@@ -0,0 +1,12 @@
+package abstraction;
+
+public class AnimalFarm {
+
+    public static final int MAX_ANIMALS = 5;
+
+    public static void main(String[] args) {
+        Animal animal;
+
+
+    }
+}
